ffb0db62f36494c3264cbfe616a68f798337b3e8
Abbosbekboss <kukanovabbosbek@gmail.com>
2022-11-10 20:59:09 +0500

    homework3

--- index.js
+++ index.js
@@ -2,79 +2,45 @@ var ism;
 ism = prompt('Ismingizni kiriting:', 'Ism');
 console.log(ism);
 
-var otm;
+var familiya;
+familiya = prompt('Familiyangizni kiriting', 'Familiya');
+console.log(familiya);
 
-var a = 'TDIU';
-var b = 'TDMI';
-var c = 'MU';
+var data;
+data = prompt('Tug`ilgan sanangizni kiriting:', 'dd. mm. yyyy.');
+console.log(data);
 
-otm = prompt(`${ism}  Qaysi OTMni Tanlaysiz:  1. ${a} 2. ${b} 3. ${c} `, 'OTMni kiriting!(Katta harflar bilan)');
-console.log(otm);
-
-var ball;
-ball = prompt(`${ism} Balingizni kiriting: `, 'Ball');
-console.log(ball);
-
+var joy;
+joy = prompt('Tug`ilgan viloyatingizni kiriting', 'Sirdaryo');
+console.log(joy);
 
 
-var grand = 'Siz talabalikka grandga qabul qilindingiz';
-var kontrakt = 'Siz talabalikka kontrakt qabul qilindingiz';
-var superkontrakt = 'Siz talabalikka qabul qilinmadingiz';
+var raqam;
+raqam = prompt('Telefon raqamingizni kiriting', '+998XXYYYYYYY');
+console.log(raqam);
 
-var not = "siz noto'g'ri malumot kiritdingiz"
 
-if (a == otm) {
+var email;
+email = prompt('Email manzili=ngizni kiriting', 'quyoncha.@gmail.com');
+console.log(email); 
 
-    if (ball >= 150) {
-        alert(grand);
-    }
 
-    else if (120 <= ball) {
-        alert(kontrakt);
-    }
 
-    else {
-        alert(superkontrakt);
-    }
+var elName = document.querySelector('#name');
+elName.textContent = `Name: ${ism}`;
 
-}
+var elLastName = document.querySelector('#lastname');
+elLastName.textContent = `Lastname: ${familiya}`;
 
-else{
-    alert(not)
-}
+var eldate = document.querySelector('#date');
+eldate.textContent = `Date of birth: ${data}`;
 
-if (b == otm) {
 
-    if (ball >= 160) {
-        alert(grand);
-    }
-    else if (100 <= ball) {
-        alert(kontrakt);
-    }
-    else {
-        alert(superkontrakt);
-    }
+var elhome = document.querySelector('#home');
+elhome.textContent = `Hometown: ${joy}`;
 
-}
+var elnomber = document.querySelector('#nomber');
+elnomber.textContent = `Tel: ${raqam}`;
 
-else{
-    alert(not)
-}
-
-if (c == otm) {
-
-    if (ball >= 180) {
-        alert(grand);
-    }
-    else if (168 <= ball) {
-        alert(kontrakt);
-    }
-    else {
-        alert(superkontrakt);
-    }
-
-}
-
-else{
-    alert(not)
-}
+var elemail = document.querySelector('#email');
+elemail.textContent = `Email: ${email}`;
